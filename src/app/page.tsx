dbf1ae6b944726ac02b3209e69dfb434e8e98b2e
neuromagus <neuromagus@mail.ru>
2024-09-14 03:22:13 +0300

    fix indent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import { auth, signOut } from "@/auth";
 import { Button } from "@nextui-org/react";
-import Link from "next/link";
 import { FaRegSmile } from "react-icons/fa";
 
 export default async function Home() {
     const session = await auth()
     return (
         <div>
-
             <h3 className="text-2xl font-semibold">User session data:</h3>
             {session ? (
                 <div>
@@ -31,7 +29,6 @@ export default async function Home() {
             ) : (
                 <div>Not signed in</div>
             )}
-
         </div>
     )
 }
