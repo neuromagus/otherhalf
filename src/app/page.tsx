402bd0dfe11eaaace0634611eb9dfc742dbfe56e
neuromagus <neuromagus@mail.ru>
2024-09-14 03:21:42 +0300

    checking session and signout (in main page)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,37 @@
+import { auth, signOut } from "@/auth";
 import { Button } from "@nextui-org/react";
 import Link from "next/link";
 import { FaRegSmile } from "react-icons/fa";
 
-export default function Home() {
+export default async function Home() {
+    const session = await auth()
     return (
         <div>
-            <Button
-                color="primary"
-                variant="bordered"
-                startContent={<FaRegSmile size={20} />}
-                as={Link}
-                href="/members"
-            >
 
-                click
-            </Button>
+            <h3 className="text-2xl font-semibold">User session data:</h3>
+            {session ? (
+                <div>
+                    <pre>{JSON.stringify(session, null, 2)}</pre>
+
+                    <form action={async () => {
+                        "use server"
+                        await signOut()
+                    }}>
+                        <Button
+                            type="submit"
+                            color="primary"
+                            variant="bordered"
+                            startContent={<FaRegSmile size={20} />}
+                            href="/members"
+                        >
+                            SignOut
+                        </Button>
+                    </form>
+                </div>
+            ) : (
+                <div>Not signed in</div>
+            )}
+
         </div>
     )
 }
