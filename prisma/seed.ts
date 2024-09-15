50a03479601e654a7f06d464d3920c9f9bc834ed
neuromagus <neuromagus@mail.ru>
2024-09-15 10:30:09 +0300

    seeded data to the DB

---
+++ prisma/seed.ts
@@ -0,0 +1,46 @@
+import { PrismaClient } from "@prisma/client";
+import { membersData } from "./membersData";
+import { hash } from "bcryptjs";
+
+const prisma = new PrismaClient()
+
+async function seedMembers() {
+    return membersData.map(async member => prisma.user.create({
+        data: {
+            email: member.email,
+            emailVerified: new Date(), // technically, all Emails verified :)
+            name: member.name,
+            passwordHash: await hash("simplepassword", 10), // TODO: later need add to env
+            image: member.image,
+            member: {
+                create: {
+                    dateOfBirth: new Date(member.dateOfBirth),
+                    gender: member.gender,
+                    name: member.name,
+                    created: new Date(member.created),
+                    updated: new Date(member.lastActive),
+                    description: member.description,
+                    city: member.city,
+                    country: member.country,
+                    image: member.image,
+                    photos: {
+                        create: {
+                            url: member.image
+                        }
+                    }
+                }
+            }
+        }
+    }))
+}
+
+async function main() {
+    await seedMembers()
+}
+
+main().catch(error => {
+    console.error(error)
+    process.exit(1)
+}).finally(async () => {
+    await prisma.$disconnect()
+})
