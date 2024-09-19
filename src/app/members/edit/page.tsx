3aa2eede751b205024cffcc1bf16a46b82c0b91f
neuromagus <neuromagus@mail.ru>
2024-09-19 12:00:56 +0300

    create a form for editing user profile (todo, now submit work on console.log, later need add server side update)

--- src/app/members/edit/page.tsx
+++ src/app/members/edit/page.tsx
@@ -1,5 +1,24 @@
-export default function MemberEditPage() {
-  return (
-    <div>MemberEditPage</div>
-  )
+import { CardHeader, Divider, CardBody } from "@nextui-org/react";
+import EditForm from "./EditForm";
+import { getAuthUserId } from "@/app/actions/authActions";
+import { getMemberByUserId } from "@/app/actions/memberActions";
+import { notFound } from "next/navigation";
+
+export default async function MemberEditPage() {
+    const userId = await getAuthUserId()
+    const member = await getMemberByUserId(userId)
+
+    if (!member) return notFound()
+
+    return (
+        <>
+            <CardHeader className="text-2xl font-semibold text-secondary">
+                Edit Profile
+            </CardHeader>
+            <Divider />
+            <CardBody>
+                <EditForm member={member} />
+            </CardBody>
+        </>
+    )
 }
