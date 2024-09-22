f077923adcea75525cdda7cb7501e622a8ab45d4
neuromagus <neuromagus@mail.ru>
2024-09-22 11:23:12 +0300

    shows messages (styles)

--- src/app/members/[userId]/chat/MessageBox.tsx
+++ src/app/members/[userId]/chat/MessageBox.tsx
@@ -9,6 +9,13 @@ type Props = {
 
 export default function MessageBox({ message, currentUserId }: Props) {
     const isCurrentUserSender = message.senderId === currentUserId
+    const messageContentClasses = clsx(
+        "flex flex-col w-[50%] px-2 py-1",
+        {
+            "rounded-l-xl rounded-tr-xl text-white bg-blue-100": isCurrentUserSender,
+            "rounded-r-xl rounded-tl-xl border-gray-200 bg-green-100": !isCurrentUserSender
+        }
+    )
 
     const renderAvatar = () => (
         <Avatar
@@ -18,14 +25,37 @@ export default function MessageBox({ message, currentUserId }: Props) {
         />
     )
 
+    const renderMessageContent = () => (
+        <div className={messageContentClasses}>
+            {renderMessageHeader()}
+            <p className="text-sm py-3 text-gray-900">{message.text}</p>
+        </div>
+    )
+
+    const renderMessageHeader = () => (
+        <div className={clsx("flex items-center w-full", {
+            "justify-between": isCurrentUserSender
+        })}>
+            {message.dateRead && message.recepientId !== currentUserId ? (
+                <span className="text-xs text-black text-italic">
+                    (Read 4 mins ago)
+                </span>
+            ) : <div></div>}
+            <div className="flex">
+                <span className="text-sm font-semibold text-gray-900">{message.senderName}</span>
+                <span className="text-sm text-gray-500 ml-2">{message.created}</span>
+            </div>
+        </div>
+    )
+
     return (
         <div className="grid grid-rows-1">
             <div className={clsx("flex gap-2 mb-3", {
-                "justify-end text-right" : isCurrentUserSender,
-                "justify-start" : !isCurrentUserSender
+                "justify-end text-right": isCurrentUserSender,
+                "justify-start": !isCurrentUserSender
             })}>
                 {!isCurrentUserSender && renderAvatar()}
-                <div>Message content</div>
+                {renderMessageContent()}
                 {isCurrentUserSender && renderAvatar()}
             </div>
         </div>
