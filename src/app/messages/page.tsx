82d93ed1c48d95a5b4dbb7fc79b8dfcc121384ce
neuromagus <neuromagus@mail.ru>
2024-09-22 11:51:35 +0300

    created messageSidbar for messages (myTopNav->Messages)

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,6 +1,15 @@
+import MessageSidebar from "./MessageSidebar";
+
 export default function MessagesPage () {
     return (
-        <>MessagesPage</>
+        <div className="grid grid-cols-12 gap-5 h-[80vh] mt-10">
+            <div className="col-span-2">
+                <MessageSidebar />
+            </div>
+            <div className="col-span-10">
+                Message table goes here
+            </div>
+        </div>
     )
 }
 
