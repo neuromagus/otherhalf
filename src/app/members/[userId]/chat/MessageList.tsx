5cef825763d9b8a3d09fef40bab5ed300107a18c
neuromagus <neuromagus@mail.ru>
2024-09-24 15:00:42 +0300

    added chat realtime communication in client side (pusher subscribe)

---
+++ src/app/members/[userId]/chat/MessageList.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { MessageDto } from "@/types"
+import { useCallback, useEffect, useState } from "react"
+import MessageBox from "./MessageBox"
+import { pusherClient } from "@/lib/pusher"
+
+type Props = {
+    initialMessages: MessageDto[]
+    currentUserId: string
+    chatId: string
+}
+
+export default function MessageList({ initialMessages, currentUserId, chatId }: Props) {
+    const [messages, setMessages] = useState(initialMessages)
+
+    const handleNewMessage = useCallback((message: MessageDto) => {
+        setMessages(prevState => {
+            return [...prevState, message]
+        })
+    }, [])
+
+    useEffect(() => {
+        const channel = pusherClient.subscribe(chatId)
+        channel.bind("message:new", handleNewMessage)
+
+        return () => {
+            channel.unsubscribe()
+            channel.unbind("message:new", handleNewMessage)
+        }
+    }, [chatId, handleNewMessage])
+
+    return (
+        <div>
+            {messages.length === 0 ? "No messages to display" : (
+                <div>
+                    {messages.map(message => (
+                        <MessageBox key={message.id} message={message} currentUserId={currentUserId} />
+                    ))}
+                </div>
+            )}
+        </div>
+    )
+}
