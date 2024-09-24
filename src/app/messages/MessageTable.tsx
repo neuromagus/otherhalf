2c467493507bae7f988bdcdd3ac2669b9a8ed38b
neuromagus <neuromagus@mail.ru>
2024-09-24 11:31:00 +0300

    add to messages avatar, truncate long messages and delete button (TODO: add functionality for deleting message, and thinking about removing or not from other users this message)

--- src/app/messages/MessageTable.tsx
+++ src/app/messages/MessageTable.tsx
@@ -1,9 +1,14 @@
 "use client"
 
 import { MessageDto } from "@/types"
-import { Card, getKeyValue, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react"
+import {
+    Avatar, Button, Card, getKeyValue,
+    Table, TableBody, TableCell,
+    TableColumn, TableHeader, TableRow
+} from "@nextui-org/react"
 import { useSearchParams, useRouter } from "next/navigation"
-import { Key } from "react"
+import { Key, useCallback } from "react"
+import { AiFillDelete } from "react-icons/ai"
 
 type Props = {
     messages: MessageDto[]
@@ -18,6 +23,7 @@ export default function MessageTable({ messages }: Props) {
         { key: isOutbox ? "recipientName" : "senderName", label: isOutbox ? "Recipient" : "Sender" },
         { key: "text", label: "Message" },
         { key: "created", label: isOutbox ? "Date sent" : "Date received" },
+        { key: "actions", label: "Actions" }
     ]
 
     const handleRowSelect = (key: Key) => {
@@ -25,6 +31,38 @@ export default function MessageTable({ messages }: Props) {
         const url = isOutbox ? `/members/${message?.recipientId}` : `/members/${message?.senderId}`
         router.push(url + "/chat")
     }
+
+    const renderCell = useCallback((item: MessageDto, colmunKey: keyof MessageDto) => {
+        const cellValue = item[colmunKey]
+
+        switch (colmunKey) {
+            case "recipientName":
+            case "senderName":
+                return (
+                    <div className={`flex items-center gap-2 cursor-pointer 
+                                    ${!item.dateRead && !isOutbox ? "font-semibold" : ""}`}>
+                        <Avatar
+                            alt="Image of member"
+                            src={(isOutbox ? item.recipientImage : item.senderImage) || "/image/user.png"}
+                        />
+                        <span>{cellValue}</span>
+                    </div>
+                )
+            case "text":
+                return (
+                    <div className="truncate max-w-[200px] md:max-w-[400px] lg:max-w-[500px]">{cellValue}</div>
+                )
+            case "created":
+                return cellValue
+
+            default:
+                return (
+                    <Button isIconOnly variant="light">
+                        <AiFillDelete size={24} className="text-danger" />
+                    </Button>
+                )
+        }
+    }, [isOutbox])
 
     return (
         <Card className="flex flex-col gap-3 h-[80vh] overflow-auto mr-4">
@@ -42,9 +80,7 @@ export default function MessageTable({ messages }: Props) {
                         <TableRow key={item.id} className="cursor-pointer">
                             {colmunKey => (
                                 <TableCell>
-                                    <div className={`${!item.dateRead && !isOutbox ? "font-semibold" : ""}`}>
-                                        {getKeyValue(item, colmunKey)}
-                                    </div>
+                                    {renderCell(item, colmunKey as keyof MessageDto)}
                                 </TableCell>
                             )}
                         </TableRow>
