1e95894446c6fa0be217b1f370e91681a7043470
neuromagus <neuromagus@mail.ru>
2024-09-27 04:37:17 +0300

    fix libs

--- src/app/api/pusher-auth/route.ts
+++ src/app/api/pusher-auth/route.ts
@@ -1,25 +1,27 @@
-import { auth } from "@/auth";
-import { pusherServer } from "@/lib/pusher";
-import { NextResponse } from "next/server";
+import { auth } from '@/auth';
+import { pusherServer } from '@/lib/pusher';
+import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
     try {
-        const session = await auth()
+        const session = await auth();
 
-        if(!session?.user?.id) return new Response("Unauthorized", {status: 401})
+        if (!session?.user?.id) {
+            return new Response('Unauthorised', { status: 401 })
+        }
 
-        const body = await request.formData()
+        const body = await request.formData();
 
-        const socketId = body.get("socket_id") as string
-        const channel = body.get("channel_name") as string
+        const socketId = body.get('socket_id') as string;
+        const channel = body.get('channel_name') as string;
         const data = {
             user_id: session.user.id
         }
 
-        const authResponse = pusherServer.authorizeChannel(socketId, channel, data)
+        const authResonse = pusherServer.authorizeChannel(socketId, channel, data);
 
-        return NextResponse.json(authResponse)
+        return NextResponse.json(authResonse);
     } catch (error) {
-        
+
     }
 }
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,15 +1,18 @@
 "use client"
 
-import { NextUIProvider } from "@nextui-org/react";
-import { ReactNode } from "react";
-import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css'
+import { usePresenceChannel } from "@/hooks/usePresenceChannel"
+import { NextUIProvider } from "@nextui-org/react"
+import { ReactNode } from "react"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 export default function Providers({ children }: { children: ReactNode }) {
+    usePresenceChannel()
+
     return (
         <NextUIProvider>
             <ToastContainer position="bottom-right" hideProgressBar autoClose={5000} className="z-50" />
-                {children}
+            {children}
         </NextUIProvider>
     )
 }
