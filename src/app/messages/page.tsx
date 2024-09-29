6a45c1a112e20a65a7796562f8397e643f494444
neuromagus <neuromagus@mail.ru>
2024-09-30 00:34:53 +0300

    remove debug info

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -4,7 +4,7 @@ import MessageTable from "./MessageTable";
 
 export default async function MessagesPage ({searchParams}: {searchParams: {container: string}}) {
     const messages = await getMessagesByContainer(searchParams.container)
-    console.log(messages)
+
     return (
         <div className="grid grid-cols-12 gap-5 h-[80vh] mt-10">
             <div className="col-span-2">
