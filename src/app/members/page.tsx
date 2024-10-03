11c6fa7891c49001b6ffeed822efe0b1b923e66a
neuromagus <neuromagus@mail.ru>
2024-10-03 23:46:36 +0300

    fixed styling and added a new component - pagination

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,3 +1,4 @@
+import PaginationComponent from "@/components/PaginationComponent";
 import { fetchCurrentUserLikeIds } from "../actions/likeActions";
 import { getMembers } from "../actions/memberActions";
 import MemberCard from "./MemberCard";
@@ -7,10 +8,13 @@ export default async function MembersPage() {
     const likeIds = await fetchCurrentUserLikeIds()
 
     return (
-        <div className="mt-10 px-4 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8">
-            {members && members.map(member => (
-                <MemberCard member={member} key={member.id} likeIds={likeIds} />
-            ))}
-        </div>
+        <>
+            <div className="mt-6 px-4 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-8">
+                {members && members.map(member => (
+                    <MemberCard member={member} key={member.id} likeIds={likeIds} />
+                ))}
+            </div>
+            <PaginationComponent />
+        </>
     )
 }
