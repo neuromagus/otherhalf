56ca34540082ccd03df785fe43277809953de741
neuromagus <neuromagus@mail.ru>
2024-10-03 23:20:14 +0300

    created and added in NavBar Filters component (order by, gender, age etc)

--- src/components/navbar/MyNavBar.tsx
+++ src/components/navbar/MyNavBar.tsx
@@ -5,6 +5,7 @@ import NavLink from "./NavLink";
 import { auth } from "@/auth";
 import UserMenu from "./UserMenu";
 import { getUserInfoForNavbar } from "@/app/actions/userActions";
+import Filters from "./Filters";
 
 export default async function MyNavBar() {
     const session = await auth()
@@ -12,40 +13,45 @@ export default async function MyNavBar() {
     const userInfo = session?.user && await getUserInfoForNavbar()
 
     return (
-        <Navbar
-            maxWidth="xl"
-            className="bg-gradient-to-r from-purple-400 to-purple-700"
-            classNames={{
-                item: [
-                    "data-[active=true]:text-yellow-200",
-                    "text-xl",
-                    "text-white",
-                    "uppercase"
-                ]
-            }}
-        >
-            <NavbarBrand as={Link} href="/">
-                <TiHeartHalfOutline size={40} className="text-gray-200" />
-                <div className="font-bold text-3xl flex">
-                    <span className="text-gray-900">Other</span>
-                    <span className="text-gray-200">Half</span>
-                </div>
-            </NavbarBrand>
-            <NavbarContent justify="center">
-                <NavLink href="/members" label="Matches" />
-                <NavLink href="/lists" label="Lists" />
-                <NavLink href="/messages" label="Messages" />
-            </NavbarContent>
-            <NavbarContent justify="end">
-                {userInfo ? (
-                    <UserMenu userInfo={userInfo} />
-                ) : (
-                    <>
-                        <Button as={Link} href="/login" variant="bordered" className="text-white">Login</Button>
-                        <Button as={Link} href="/register" variant="bordered" className="text-white">Register</Button>
-                    </>
-                )}
-            </NavbarContent>
-        </Navbar>
+        <>
+            <Navbar
+                maxWidth="xl"
+                className="bg-gradient-to-r from-purple-400 to-purple-700"
+                classNames={{
+                    item: [
+                        "data-[active=true]:text-yellow-200",
+                        "text-xl",
+                        "text-white",
+                        "uppercase"
+                    ]
+                }}
+            >
+                <NavbarBrand as={Link} href="/">
+                    <TiHeartHalfOutline size={40} className="text-gray-200" />
+                    <div className="font-bold text-3xl flex">
+                        <span className="text-gray-900">Other</span>
+                        <span className="text-gray-200">Half</span>
+                    </div>
+                </NavbarBrand>
+                <NavbarContent justify="center">
+                    <NavLink href="/members" label="Matches" />
+                    <NavLink href="/lists" label="Lists" />
+                    <NavLink href="/messages" label="Messages" />
+                </NavbarContent>
+                <NavbarContent justify="end">
+                    {userInfo ? (
+                        <UserMenu userInfo={userInfo} />
+                    ) : (
+                        <>
+                            <Button as={Link} href="/login" variant="bordered" className="text-white">Login</Button>
+                            <Button as={Link} href="/register" variant="bordered" className="text-white">
+                                Register
+                            </Button>
+                        </>
+                    )}
+                </NavbarContent>
+            </Navbar>
+            <Filters />
+        </>
     )
 }
