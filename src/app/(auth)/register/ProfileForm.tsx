69779f346f261f1da663a958e0f2e14a0327c255
neuromagus <neuromagus@mail.ru>
2024-10-06 03:10:29 +0300

    implement ProfileForm (dateOfBirth we can change to the DateInput from the latest NextUI (2.4.0), but... this version 2.2.9 did not have other bugs) maybe i update this crap later... Who knows...

--- src/app/(auth)/register/ProfileForm.tsx
+++ src/app/(auth)/register/ProfileForm.tsx
@@ -1,5 +1,69 @@
+"use client"
+
+import { Input, Select, SelectItem, Textarea } from "@nextui-org/react"
+import { format, subYears } from "date-fns"
+import { useFormContext } from "react-hook-form"
+
 export default function ProfileForm() {
+    const { register, getValues, setValue, formState: { errors } } = useFormContext()
+    const genderList = [
+        { label: "Male", value: "male" },
+        { label: "Female", value: "female" },
+    ]
+
     return (
-        <div>ProfileForm</div>
+        <div className="space-y-4">
+            <Select
+                label="Gender"
+                aria-label="Select gender"
+                variant="bordered"
+                {...register("gender")}
+                defaultSelectedKeys={getValues("gender")}
+                isInvalid={!!errors.gender}
+                errorMessage={errors.gender?.message as string}
+                onChange={e => setValue("gender", e.target.value)}
+            >
+                {genderList.map(item => (
+                    <SelectItem key={item.value} value={item.value}>
+                        {item.label}
+                    </SelectItem>
+                ))}
+            </Select>
+            <Input
+                label="Date of birth"
+                max={format(subYears(new Date(), 18), "yyyy-MM-dd")}
+                variant="bordered"
+                type="date"
+                {...register("dateOfBirth")}
+                defaultValue={getValues("dateOfBirth")}
+                isInvalid={!!errors.dateOfBirth}
+                errorMessage={errors.dateOfBirth?.message as string}
+            />
+            <Textarea
+                label="Description"
+                variant="bordered"
+                {...register("description")}
+                defaultValue={getValues("description")}
+                isInvalid={!!errors.description}
+                errorMessage={errors.description?.message as string}
+            />
+            <Input
+                label="City"
+                variant="bordered"
+                {...register("city")}
+                defaultValue={getValues("city")}
+                isInvalid={!!errors.city}
+                errorMessage={errors.city?.message as string}
+            />
+            <Input
+                label="Country"
+                variant="bordered"
+                {...register("country")}
+                defaultValue={getValues("country")}
+                isInvalid={!!errors.country}
+                errorMessage={errors.country?.message as string}
+            />
+
+        </div>
     )
 }
